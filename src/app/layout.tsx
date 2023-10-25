55bce5de4a6b4bd0a9e03fe12371de1307e2c0ed
Marco Huberts <marcohuberts@Marcos-iMac.local>
2023-10-26 00:27:05 +0200

    Add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import './globals.css';
 import localFont from '@next/font/local';
-import NavBar from './components/NavBar.tsx';
 import Head from 'next/head';
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import Footer from './components/Footer.tsx';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import { Metadata } from 'next';
 config.autoAddCss = false;
 
 const proximaNova = localFont({
@@ -26,10 +27,29 @@ const proximaNovaItalic = localFont({
   variable: '--font-proximaNovaItalic',
 });
 
-export const metadata = {
-  title: 'PoSciDon',
-  description: 'PoSciDon DAO Website',
-  
+const url = new URL('https://poscidon.com/');
+
+export const metadata: Metadata = {
+  metadataBase: url,
+  title: 'PoSciDon DAO | Personalized Medicine Funding Revolutionized',
+  description: `PoSciDon is a decentralized science DAO that
+  aims to streamline the funding process of personalized medicine research.
+  Join the revolution!
+  `,
+  viewport: "width=device-width, initial-scale=1.0",
+  alternates: {
+    canonical: "https://poscidon.com/"
+  },
+  robots: "index, follow",
+  icons: [
+          {rel: "icon", url: `${url}favicon.ico`},
+          {rel: "icon", url: `${url}favicon-16x16.png`},
+          {rel: "icon", url: `${url}favicon-32x32.png`},
+          {rel: "apple-touch-icon", url: `${url}apple-touch-icon.png`},
+          {rel: "android-chrome-192x192", url: `${url}android-chrome-192x192.png`},
+          {rel: "android-chrome-512x512", url: `${url}android-chrome-512x512.png`},
+        ],
+  manifest: `${url}manifest.json`
 };
 
 export default function RootLayout({
@@ -38,53 +58,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-    lang="en">
+    <html lang="en">
       <Head>
-        <link
-          rel="android-chrome-192x192"
-          sizes="192x192"
-          href="/android-chrome-192x192.png"
-        />
-        <link
-          rel="android-chrome-512x512"
-          sizes="512x512"
-          href="/android-chrome-512x512.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      <script src="https://kit.fontawesome.com/bfd0659090.js" crossOrigin="anonymous"></script>
+        <script
+          src="https://kit.fontawesome.com/bfd0659090.js"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body
-            className={`
+        className={`
             bg-[#FDFDFD]
             flex
             flex-col
-            items-center
             justify-center
+            items-center
             ${proximaNova.variable}  
             ${proximaNovaExtraBold.variable}  
             ${proximaNovaSemiBold.variable}  
             ${proximaNovaItalic.variable}  
             font-proxima
              `}
-          >
-        <NavBar />
+      >
+        <NavBar/>
         {children}
         <hr className="border-b-1 w-full border-gray-200"></hr>
         <Footer/>
