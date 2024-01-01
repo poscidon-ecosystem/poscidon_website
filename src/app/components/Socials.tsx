47d0dc8aed0fe8304f10ad4d1f06fd52521005fe
Marco Huberts <marcohuberts@Marcos-MacBook-Air.local>
2024-01-01 15:51:11 +0100

    Update discord link

--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -4,34 +4,52 @@ import { faDiscord, faTwitter, faTelegram } from '@fortawesome/free-brands-svg-i
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Socials() {
-    return(
-        <div
-            className="
+    return (
+      <div
+        className="
           flex
           w-full
           items-center 
           justify-center
           gap-24
           "
-          >
-            <Link
-            aria-label="Join PoSciDon DAO's Discord server" 
-            className="flex items-center justify-center"
-            href="https://discord.gg/TXZZV5KXmn" target="_blank">
-                <FontAwesomeIcon icon={faDiscord} className="fa-2x" style={{color: "#0b1dee"}}/>
-            </Link>
-            <Link
-            aria-label="Visit PoSciDon DAO's X (formerly known as Twitter) profile" 
-            className="flex h-[4rem] w-10 items-center justify-center"
-            href="https://twitter.com/poscidondao" target="_blank">
-                <FontAwesomeIcon icon={faTwitter} className="fa-2x" style={{color: "#0b1dee"}}/>
-            </Link>
-            <Link 
-            aria-label="Start the conversation in PoSciDon DAO's Telegram group"
-            className="flex h-[4rem] w-10 items-center justify-center"
-            href="https://t.me/+orlGVaLwlGVlZjI8" target="_blank">
-                <FontAwesomeIcon icon={faTelegram} className="fa-2x" style={{color: "#0b1dee"}}/>
-            </Link>
-          </div>
+      >
+        <Link
+          aria-label="Join PoSciDon DAO's Discord server"
+          className="flex items-center justify-center"
+          href="https://discord.gg/75SrHpcNSZ"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faDiscord}
+            className="fa-2x"
+            style={{ color: '#0b1dee' }}
+          />
+        </Link>
+        <Link
+          aria-label="Visit PoSciDon DAO's X (formerly known as Twitter) profile"
+          className="flex h-[4rem] w-10 items-center justify-center"
+          href="https://twitter.com/poscidondao"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className="fa-2x"
+            style={{ color: '#0b1dee' }}
+          />
+        </Link>
+        <Link
+          aria-label="Start the conversation in PoSciDon DAO's Telegram group"
+          className="flex h-[4rem] w-10 items-center justify-center"
+          href="https://t.me/+orlGVaLwlGVlZjI8"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faTelegram}
+            className="fa-2x"
+            style={{ color: '#0b1dee' }}
+          />
+        </Link>
+      </div>
     );
 }
